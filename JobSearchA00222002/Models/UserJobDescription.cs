94980dc76cd1100ae9c414ab31c88b19ef8aab79
jagdeepsingh84 <A00222002@mycambrian.ca>
2021-04-10 20:06:44 -0600

    j

--- JobSearchA00222002/Models/UserJobDescription.cs
+++ JobSearchA00222002/Models/UserJobDescription.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,11 @@ namespace JobSearchA00222002.Models
 {
     public class UserJobDescription
     {
+        public int ID { get; set; }
+
+        public string JobType { get; set; }
+
+        [StringLength(4000)]
+      public string UserRequirments { get; set; }
     }
 }
